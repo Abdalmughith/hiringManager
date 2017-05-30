4ef54a92a955778e80202047bae367d79442fa42
AbdalmuGhith Alz <abdalmugith@gamil.com>
2017-05-31 02:17:51 +0300

    we

--- controllers/nools.js
+++ controllers/nools.js
@@ -10,6 +10,7 @@ var flow = nools.compile(__dirname + '/rules.nools');
 var Person = flow.getDefined('Person');
 var skill = flow.getDefined('skill');
 var Position = flow.getDefined('position');
+var personPos = flow.getDefined('personPos');
 var Result = flow.getDefined('Result');
 
 var getpositionPersons = flow.getDefined('getpositionPersons');
@@ -21,9 +22,7 @@ var session = flow.getSession();
 var result = new Result();
 session.assert(result);
 
-
-session.assert(new Position('web devloper',[{name: 'node',score :5},{name: 'php',score :7},{name: 'html',score :1}]));
-session.assert(new Position('23v',[{name: 'php',score :1}]));
+session.assert(new Position('WEb',[{name: 'node',score :5},{name: 'php',score :7},{name: 'html',score :1}],"student",4));
 
 
 var person2 = new Person({
@@ -32,6 +31,7 @@ var person2 = new Person({
 });
 session.assert(person2);
 session.assert(new skill(person2.id,'node', 10));
+session.assert(new skill(person2.id,'experience', 10));
 session.assert(new skill(person2.id, 'php', 10));
 session.assert(new skill(person2.id, 'html', 10));
 
@@ -54,7 +54,15 @@ session.match(function(err){
     if(err){
         console.error(err.stack);
     }else{
-        console.log(result.fired);
+
+          var persones = session.getFacts(Person);
+            var skills = session.getFacts(skill);
+            var positions = session.getFacts(Position);
+            var personPos = session.getFacts(personPos);
+             console.log(persones);
+            console.log(skills);
+            console.log("positions"+positions);
+             console.log("personPos " + personPos);
         console.log("done");
     }
 })
